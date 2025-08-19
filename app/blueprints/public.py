da042d4d01bb4eab65bb78cf07c03dda70747aaf
Lucas Pecout <100285952+lucaspecout@users.noreply.github.com>
2025-08-19 13:53:17 +0200

    Update public.py

--- app/blueprints/public.py
+++ app/blueprints/public.py
@@ -9,6 +9,7 @@ import io
 from ..models import Utilisateur, Evenement, FicheImplique, Bagage, ShareLink, Ticket, Animal, utilisateur_evenement
 from .. import db
 from ..backup_utils import is_db_empty, backup_to_bytesio, wipe_db, bulk_restore
+from app.blueprints.decorators import login_required, role_required, event_member_required
 
 
 def register(bp):
